f306279f7fd4f2bbc3b5937ab7534aa815fb2f3b
Guilherme <guilhermefuturo@hotmail.com>
2017-03-25 16:39:11 -0300

    Part of Game Jam layout

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170324152037) do
+ActiveRecord::Schema.define(version: 20170322234458) do
 
   create_table "game_jams", force: :cascade do |t|
     t.string   "theme"
