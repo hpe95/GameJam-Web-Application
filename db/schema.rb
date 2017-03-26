e66b83bb9586db076342983b8769bea733774167
Felipe <felipe.osorio72@outlook.com.br>
2017-03-26 09:40:17 -0300

    Added comments CRUD

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170324131729) do
+ActiveRecord::Schema.define(version: 20170326123516) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "field"
+    t.integer  "rate"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.integer  "game_id"
+  end
+
+  add_index "comments", ["game_id"], name: "index_comments_on_game_id"
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "games", force: :cascade do |t|
     t.string   "name"
