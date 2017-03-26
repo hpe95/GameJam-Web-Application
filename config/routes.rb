7d68f9e7a6f50615e62491d73da91dc3f27c8c1b
Harrison <hpedro1195@gmail.com>
2017-03-26 19:54:17 -0300

    Create user and Create session helper

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'users/new'
+
   resources :comments
   get 'games/index'
   get 'games/show'
@@ -6,9 +10,16 @@ Rails.application.routes.draw do
   get 'games_controller/deleteKeys'
 
   get 'organizations/show'
+  get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
 
   get 'organizations/new'
-
+  resources :users
   resources :game_jams
 	
 	resources :organizations
@@ -18,21 +29,21 @@ Rails.application.routes.draw do
 
   ## config/routes.rb
 
-  devise_for :users, :skip => :registrations
-  devise_for :gamers, :developers
+  #devise_for :users, :skip => :registrations
+  #devise_for :gamers, :developers
   
   resources :games
   # routes for all users
-  authenticated :user do
-  end
+  #authenticated :user do
+  #end
 
   # routes only for users
-  authenticated :user, lambda {|u| u.type == "Gamer"} do
-  end
+  #authenticated :user, lambda {|u| u.type == "Gamer"} do
+  #end
 
   # routes only for companies
-  authenticated :user, lambda {|u| u.type == "Developer"} do
-  end
+  #authenticated :user, lambda {|u| u.type == "Developer"} do
+  #end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170326212034) do
+ActiveRecord::Schema.define(version: 20170326222238) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "field"
@@ -53,5 +53,17 @@ ActiveRecord::Schema.define(version: 20170326212034) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
+    t.string   "github"
+    t.string   "company"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
